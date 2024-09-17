a68b353fe58567b5f3d6502cab77351bf52225a4
sean <sozalpasan1@hwemail.com>
2024-09-17 13:32:03 -0700

    started writing initialize repo method

--- Git.java
+++ Git.java
@@ -1,6 +1,14 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 //im j tseting
 public class Git {
     public static void main (String [] args) {
         
+    }
+
+    public void initializeRepo(String repoName){
+        File gitFile = new File(repoName);
+        gitFile.mkdir();
     }
 }
